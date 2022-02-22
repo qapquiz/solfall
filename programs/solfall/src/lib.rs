ff3a53a16e78eb360085cf8f06bbbd2ad20d557f
0bytespeak <ariya.l@dv.co.th>
2022-02-22 16:07:01 +0700

    feat: add deposit_initialize with implementation

--- programs/solfall/src/lib.rs
+++ programs/solfall/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::Mint;
 
 declare_id!("BHeZ6pUpo8Z3d1x93mwn2SNnrddFSUYoPAdNdEfxX3d2");
 
@@ -9,8 +10,19 @@ pub mod solfall {
         Ok(())
     }
 
-    pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
+    pub fn deposit_initialize(ctx: Context<DepositInitialize>) -> Result<()> {
+        let deposit_detail = &mut ctx.accounts.deposit_detail;
+        deposit_detail.token_mint = ctx.accounts.token_mint.key();
+        deposit_detail.deposit_amount = 0;
+        deposit_detail.bump = *ctx.bumps.get("deposit_detail").unwrap();
+        deposit_detail.owner = ctx.accounts.depositor.key();
+
+        // @todo #3 create stake account for port financeg
         Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>) -> Result<()> {
+        // @todo #4 refresh reserve account
     }
 }
 
@@ -18,15 +30,41 @@ pub mod solfall {
 pub struct Initialize {}
 
 #[derive(Accounts)]
-#[instruction(deposit_amount: u64)]
-pub struct Deposit<'info> {
-    // @todo #1 use #[account] init here with seeds = [b"deposit_detail", user_pubkey]
+pub struct DepositInitialize<'info> {
+    pub token_mint: Account<'info, Mint>,
+    #[account(
+        init,
+        seeds = [
+            b"deposit_detail".as_ref(),
+            depositor.key().as_ref()
+        ],
+        bump,
+        payer = depositor,
+        space = DepositDetail::LEN,
+    )]
     pub deposit_detail: Account<'info, DepositDetail>,
+    #[account(mut)]
+    pub depositor: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+
+}
+
+// impl<'info> Deposit<'info> {
+//     fn into_init_obligation_context<'a, 'b, 'c, 'info,
+// }
 
 #[account]
 pub struct DepositDetail {
     pub token_mint: Pubkey,
     pub deposit_amount: u64,
+    pub bump: u8,
     pub owner: Pubkey,
+}
+
+impl DepositDetail {
+    pub const LEN: usize = 8 + 32 + 8 + 1 + 32;
 }
--- tests/solfall.ts
+++ tests/solfall.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { createMint } from "@solana/spl-token";
 import { Solfall } from "../target/types/solfall";
 
 describe("solfall", () => {
@@ -7,10 +8,53 @@ describe("solfall", () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Solfall as Program<Solfall>;
+  const userKeypair = anchor.web3.Keypair.generate();
+  let tokenMint: anchor.web3.PublicKey;
+
+  it("request airdrop", async () => {
+    const airdropTx = await program.provider.connection.requestAirdrop(userKeypair.publicKey, anchor.web3.LAMPORTS_PER_SOL);
+    await program.provider.connection.confirmTransaction(airdropTx);
+
+    console.log("airdropTx:", airdropTx);
+  });
+  
+  it("Create mint", async () => {
+    tokenMint = await createMint(
+      program.provider.connection,
+      userKeypair,
+      userKeypair.publicKey,
+      null,
+      9,
+    );
+
+    console.log("create mint:", tokenMint.toBase58());
+  });
 
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.rpc.initialize({});
     console.log("Your transaction signature", tx);
+  });
+
+  it("Is DepositInitialized!", async () => {
+    // @todo #2 create pda with seeds b"deposit_detail" + public key of depositor
+    const [depositDetailPda] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from(anchor.utils.bytes.utf8.encode("deposit_detail")),
+        userKeypair.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const tx = await program.methods
+      .depositInitialize()
+      .accounts({
+        tokenMint: tokenMint,
+        depositor: userKeypair.publicKey,
+      })
+      .signers([userKeypair])
+      .rpc();
+
+    console.log("DepositInitialize transaction signature", tx);
   });
 });
